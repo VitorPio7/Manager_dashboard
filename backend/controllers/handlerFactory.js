93890a54a105aa33e65fd39cc74be18a6bd97d8d
vitorpio7 <vitorvpio60@gmail.com>
2025-11-04 10:48:29 -0300

    today i did this

--- backend/controllers/handlerFactory.js
+++ backend/controllers/handlerFactory.js
@@ -60,9 +60,13 @@ exports.updateProducts = Model => catchAsync(async (req, res, next) => {
 })
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
-  const doc = await Model.findByIdAndDelete(req.params.id);
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-  if(!doc) {
-    return next(new AppError('No document found with that ID',404))
-  }
+    if (!doc) {
+        return next(new AppError('No document found with that ID', 404))
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
 }) 
